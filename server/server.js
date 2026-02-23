05d2fea8c8b41274c6587b081fc19fb11d61d597
KISHAN-SINGH <krishansingh6669@gmail.com>
2026-02-23 21:30:34 +0530

    Fix model import case sensitivity

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const Todo = require("./models/todo");
+const Todo = require("./models/Todo");
 
 const app = express();
-git 
+
 app.use(cors());
 app.use(express.json());
 
