9fb6ef70b292462b3ed8a3402dbf00667a2d1233
KISHAN-SINGH <krishansingh6669@gmail.com>
2026-02-23 22:17:05 +0530

    force rename case-2

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const Todo = require("./models/temp");
+const Todo = require("./models/todo");
 
 const app = express();
 
