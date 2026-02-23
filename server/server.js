10a97587a640068749cba5dd60565081d2e4aea8
KISHAN-SINGH <krishansingh6669@gmail.com>
2026-02-23 22:26:32 +0530

    Add root test route

--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
 // MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI)
